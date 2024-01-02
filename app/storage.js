871f6d718830d35f385fb9882ee4c9f91fe86674
Sam Plackett <samuel.plackett@eviden.com>
2024-01-02 16:41:22 +0000

    switch to blob storage

--- app/storage.js
+++ app/storage.js
@@ -1,26 +1,60 @@
-const { ShareServiceClient } = require('@azure/storage-file-share')
+const { DefaultAzureCredential } = require('@azure/identity')
+const { BlobServiceClient } = require('@azure/storage-blob')
 const { storageConfig } = require('./config')
-let demographicsShareServiceClient
-let demographicsShare
-let demographicsFolder
-let daxShareServiceClient
-let daxShare
-let daxFolder
+let blobServiceClient
+let containersInitialised
+let foldersInitialised
 
-if (storageConfig.enabled) {
-  demographicsShareServiceClient = ShareServiceClient.fromConnectionString(storageConfig.demographicsShareConnectionString)
-  demographicsShare = demographicsShareServiceClient.getShareClient(storageConfig.demographicsShareName)
-  demographicsFolder = demographicsShare.getDirectoryClient(storageConfig.demographicsFolder)
+if (storageConfig.useConnectionStr) {
+  console.log('Using connection string for BlobServiceClient')
+  blobServiceClient = BlobServiceClient.fromConnectionString(storageConfig.connectionString)
+} else {
+  console.log('Using DefaultAzureCredential for BlobServiceClient')
+  const uri = `https://${storageConfig.storageAccount}.blob.core.windows.net`
+  blobServiceClient = new BlobServiceClient(uri, new DefaultAzureCredential())
+}
 
-  daxShareServiceClient = ShareServiceClient.fromConnectionString(storageConfig.daxShareConnectionString)
-  daxShare = daxShareServiceClient.getShareClient(storageConfig.daxShareName)
-  daxFolder = daxShare.getDirectoryClient(storageConfig.daxFolder)
+const demographicsContainer = blobServiceClient.getContainerClient(storageConfig.demographicsContainer)
+const daxContainer = blobServiceClient.getContainerClient(storageConfig.daxContainer)
+
+const initialiseContainers = async () => {
+  if (storageConfig.enabled) {
+    if (storageConfig.createContainers) {
+      console.log('Making sure blob containers exist')
+      await demographicsContainer.createIfNotExists()
+      await daxContainer.createIfNotExists()
+      console.log('Containers ready')
+    }
+    foldersInitialised ?? await initialiseFolders()
+    containersInitialised = true
+  }
+}
+
+const initialiseFolders = async () => {
+  console.log('Making sure folders exist')
+  const placeHolderText = 'Placeholder'
+  const demographicsClient = demographicsContainer.getBlockBlobClient(`${storageConfig.demographicsFolder}/default.txt`)
+  await demographicsClient.upload(placeHolderText, placeHolderText.length)
+  const daxClient = daxContainer.getBlockBlobClient(`${storageConfig.daxFolder}/default.txt`)
+  await daxClient.upload(placeHolderText, placeHolderText.length)
+  foldersInitialised = true
+  console.log('Folders ready')
+}
+
+const getBlob = async (filename, contName, folder) => {
+  const fileContainer = contName === 'demographics' ? demographicsContainer : daxContainer
+  if (!folder) {
+    folder = contName === 'demographics' ? storageConfig.demographicsFolder : storageConfig.daxFolder
+  }
+  containersInitialised ?? await initialiseContainers()
+  return fileContainer.getBlockBlobClient(`${folder}/${filename}`)
 }
 
 const getDemographicsFiles = async () => {
+  containersInitialised ?? await initialiseContainers()
+
   const fileList = []
-  const dirIter = demographicsFolder.listFilesAndDirectories()
-  for await (const item of dirIter) {
+  for await (const item of demographicsContainer.listBlobsFlat({ prefix: storageConfig.demographicsFolder })) {
     console.log(`Found item: ${item.name}`)
 
     // Awaiting confirmation on file naming convention
@@ -32,21 +66,20 @@ const getDemographicsFiles = async () => {
   return fileList
 }
 
-const downloadFile = async (filename) => {
-  const file = demographicsFolder.getFileClient(filename)
+const downloadFile = async (filename, contName) => {
+  const file = await getBlob(filename, contName)
   const downloaded = await file.downloadToBuffer()
   return downloaded.toString()
 }
 
-const uploadFile = async (filename, content) => {
-  const file = daxFolder.getFileClient(filename)
-  await file.create(content.length)
-  await file.uploadRange(content, 0, content.length)
+const uploadFile = async (filename, content, contName) => {
+  const blobClient = await getBlob(filename, contName)
+  await blobClient.upload(content, content.length)
 }
 
-const deleteFile = async (filename) => {
-  const file = demographicsFolder.getFileClient(filename)
-  await file.delete()
+const deleteFile = async (filename, contName) => {
+  const blobClient = await getBlob(filename, contName)
+  await blobClient.delete()
 }
 
 module.exports = {

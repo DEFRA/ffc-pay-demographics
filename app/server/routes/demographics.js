562cc0042a94937f7b154a50cf1f275f61954b84
Sam Plackett <samuel.plackett@eviden.com>
2024-04-30 11:59:10 +0100

    attempt to handle handshake event

--- app/server/routes/demographics.js
+++ app/server/routes/demographics.js
@@ -7,6 +7,18 @@ module.exports = [{
   options: {
     handler: async (request, h) => {
       try {
+        const header = request.get('Aeg-Event-Type')
+        if (header && header === 'SubscriptionValidation') {
+          const event = request.body[0]
+          const isValidationEvent = event && event.data &&
+                                    event.data.validationCode &&
+                                    event.eventType && event.eventType === 'Microsoft.EventGrid.SubscriptionValidationEvent'
+          if (isValidationEvent) {
+            return h.send({
+              validationResponse: event.data.validationCode
+            })
+          }
+        }
         console.log('Received demographics event:', request.payload)
         await processDemographicsMessage(request.payload)
         return h.response().code(200)

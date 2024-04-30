8d6ecd57eaba2e3260c3a70a41a2aae50e5ff346
Sam Plackett <samuel.plackett@eviden.com>
2024-04-30 14:47:07 +0100

    rework solution to use handle subscription validation messages

--- app/server/routes/demographics.js
+++ app/server/routes/demographics.js
@@ -7,20 +7,19 @@ module.exports = [{
   options: {
     handler: async (request, h) => {
       try {
-        const header = request.get('Aeg-Event-Type')
-        if (header && header === 'SubscriptionValidation') {
-          const event = request.body[0]
-          const isValidationEvent = event && event.data &&
-                                    event.data.validationCode &&
-                                    event.eventType && event.eventType === 'Microsoft.EventGrid.SubscriptionValidationEvent'
-          if (isValidationEvent) {
-            return h.send({
-              validationResponse: event.data.validationCode
-            })
+        console.log('Received request from Event Grid')
+        for (const event of request.payload) {
+          const body = event
+          // Deserialize the event data into the appropriate type based on event type
+          if (body.data && body.eventType === 'Microsoft.EventGrid.SubscriptionValidationEvent') {
+            console.log(`Got SubscriptionValidation event data, validation code: ${body.data.validationCode}, topic: ${body.topic}`)
+            const code = body.data.validationCode
+            return h.response({ ValidationResponse: code }).code(200)
+          } else {
+            console.log('Received demographics event:', body)
+            await processDemographicsMessage(body)
           }
         }
-        console.log('Received demographics event:', request.payload)
-        await processDemographicsMessage(request.payload)
         return h.response().code(200)
       } catch (error) {
         console.error('Error processing demographics event:', error)

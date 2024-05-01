273b6d0c7b482603371dadca219820fd75cc0cf4
John Watson <john.watson1@defra.gov.uk>
2024-05-01 10:02:48 +0100

    Update tests

--- test/unit/processing/process-demographics-message.test.js
+++ test/unit/processing/process-demographics-message.test.js
@@ -5,34 +5,28 @@ const processFile = require('../../../app/processing/process-file')
 jest.mock('../../../app/processing/get-file-name-from-url')
 const { getFileNameFromUrl } = require('../../../app/processing/get-file-name-from-url')
 const { processDemographicsMessage } = require('../../../app/processing/process-demographics-message')
-let receiver
+
 const message = {
-  body: [{
+  body: {
     data: {
       url: 'https://url/to/file.txt'
     }
-  }]
+  }
 }
 
 describe('process demographics message', () => {
   beforeEach(() => {
-    receiver = {
-      completeMessage: jest.fn()
-    }
-  })
-
-  afterEach(() => {
     jest.clearAllMocks()
   })
 
   test('calls getFileNameFromUrl', async () => {
-    await processDemographicsMessage(message, receiver)
+    await processDemographicsMessage(message.body)
     expect(getFileNameFromUrl).toHaveBeenCalledWith('https://url/to/file.txt')
   })
 
   test('calls processFile', async () => {
     getFileNameFromUrl.mockReturnValue('file.txt')
-    await processDemographicsMessage(message, receiver)
+    await processDemographicsMessage(message.body)
     expect(processFile).toHaveBeenCalledWith('file.txt')
   })
 })

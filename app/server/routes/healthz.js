ef6522b98f3a5f0eea2fc6e6ddd6049a2ea54643
Sam Plackett <samuel.plackett@eviden.com>
2024-07-01 17:44:47 +0100

    refactor for hitachi service bus

--- app/server/routes/healthz.js
+++
@@ -1,9 +0,0 @@
-const { GET } = require('../../constants/methods')
-
-module.exports = {
-  method: GET,
-  path: '/healthz',
-  handler: (request, h) => {
-    return h.response('ok').code(200)
-  }
-}

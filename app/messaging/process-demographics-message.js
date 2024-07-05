3f6548c6b3acb91a349d41ac05123f49eef2225a
Sam Plackett <samuel.plackett@eviden.com>
2024-07-05 16:39:13 +0100

    amend config

--- app/messaging/process-demographics-message.js
+++ app/messaging/process-demographics-message.js
@@ -5,7 +5,6 @@ const processDemographicsMessage = async (message, receiver) => {
   try {
     const filePath = message.body[0].data.url
     const file = getFileNameFromUrl(filePath)
-    console.log(file)
     await processFile(file)
     await receiver.completeMessage(message)
   } catch (err) {

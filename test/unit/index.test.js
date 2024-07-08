2ab3579ea60d760ef048b142367490f09761bab8
Sam Plackett <samuel.plackett@eviden.com>
2024-07-08 10:49:00 +0100

    fix config imports

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,16 +1,9 @@
 jest.mock('../../app/processing')
 const { start: mockStart } = require('../../app/processing')
 
-jest.mock('../../app/server')
-const { start: mockServStart } = require('../../app/server')
-
 describe('start', () => {
   beforeEach(() => {
     require('../../app')
-  })
-
-  test('should start server', () => {
-    expect(mockServStart).toHaveBeenCalled()
   })
 
   test('should start processing', () => {

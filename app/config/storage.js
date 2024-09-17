de07c6b0fe8cff71926952a0a7540ecedf7a1d2f
Sam Plackett <samuel.plackett@eviden.com>
2024-09-17 17:28:54 +0100

    add storage config for fcp dax folder

--- app/config/storage.js
+++ app/config/storage.js
@@ -3,7 +3,9 @@ const Joi = require('joi')
 const schema = Joi.object({
   enabled: Joi.boolean().default(true),
   connectionString: Joi.string().when('useConnectionStr', { is: true, then: Joi.required(), otherwise: Joi.allow('').optional() }),
+  daxConnectionString: Joi.string().when('useDaxConnectionStr', { is: true, then: Joi.required(), otherwise: Joi.allow('').optional() }),
   storageAccount: Joi.string().required(),
+  daxStorageAccount: Joi.string().required(),
   demographicsContainer: Joi.string().required(),
   demographicsClientId: Joi.string().allow('').optional(),
   demographicsClientSecret: Joi.string().allow('').optional(),
@@ -13,22 +15,26 @@ const schema = Joi.object({
   daxOutboundFolder: Joi.string().required(),
   archiveFolder: Joi.string().required(),
   useConnectionStr: Joi.boolean().default(false),
+  useDaxConnectionStr: Joi.boolean().default(false),
   createContainers: Joi.boolean().default(false)
 })
 
 const config = {
   enabled: process.env.ENABLED,
   connectionString: process.env.DEMOGRAPHICS_STORAGE_CONNECTION_STRING,
+  daxConnectionString: process.env.AZURE_STORAGE_CONNECTION_STRING,
   storageAccount: process.env.DEMOGRAPHICS_STORAGE_ACCOUNT_NAME,
+  daxStorageAccount: process.env.AZURE_STORAGE_ACCOUNT_NAME,
   demographicsContainer: process.env.DEMOGRAPHICS_STORAGE_SHARE_NAME,
   demographicsClientId: process.env.DEMOGRAPHICS_CLIENT_ID,
   demographicsClientSecret: process.env.DEMOGRAPHICS_CLIENT_SECRET,
   demographicsTenantId: process.env.DEMOGRAPHICS_TENANT_ID,
-  daxContainer: 'dax',
-  daxFolder: process.env.DAX_STORAGE_FOLDER_NAME,
+  daxContainer: process.env.AZURE_STORAGE_CONTAINER,
+  daxFolder: process.env.AZURE_STORAGE_FOLDER,
   daxOutboundFolder: process.env.DAX_STORAGE_OUTBOUND_FOLDER_NAME,
   archiveFolder: 'archive',
-  useConnectionStr: process.env.AZURE_STORAGE_USE_CONNECTION_STRING,
+  useConnectionStr: process.env.DEMOGRAPHICS_STORAGE_USE_CONNECTION_STRING,
+  useDaxConnectionStr: process.env.AZURE_STORAGE_USE_CONNECTION_STRING,
   createContainers: process.env.AZURE_STORAGE_CREATE_CONTAINERS
 }
 

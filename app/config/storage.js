471e48f758b3486969120fdf44596dc910b2cb34
Oliver Lewington <oliver.lewington@eviden.com>
2025-03-04 11:08:36 +0000

    Commiting changes to Provision.Azure.Yaml

--- app/config/storage.js
+++ app/config/storage.js
@@ -16,7 +16,8 @@ const schema = Joi.object({
   archiveFolder: Joi.string().required(),
   useConnectionStr: Joi.boolean().default(false),
   useDaxConnectionStr: Joi.boolean().default(false),
-  createContainers: Joi.boolean().default(false)
+  createContainers: Joi.boolean().default(false),
+  managedIdentityClientId: Joi.string().optional()
 })
 
 const config = {
@@ -35,7 +36,8 @@ const config = {
   archiveFolder: 'archive',
   useConnectionStr: process.env.DEMOGRAPHICS_STORAGE_USE_CONNECTION_STRING,
   useDaxConnectionStr: process.env.AZURE_STORAGE_USE_CONNECTION_STRING,
-  createContainers: process.env.AZURE_STORAGE_CREATE_CONTAINERS
+  createContainers: process.env.AZURE_STORAGE_CREATE_CONTAINERS,
+  managedIdentityClientId: process.env.AZURE_CLIENT_ID
 }
 
 const result = schema.validate(config, {

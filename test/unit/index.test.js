fde8a379d7e722a926bd811b91b2599c9410205e
Oliver Lewington <oliver.lewington@eviden.com>
2025-03-12 17:12:40 +0000

    Adding feature flag to allow service to be disabled

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,43 +1,83 @@
+const { processingConfig } = require('../../app/config')
+
+jest.mock('log-timestamp', () => jest.fn())
 jest.mock('../../app/insights', () => ({
   setup: jest.fn()
 }))
-jest.mock('log-timestamp', () => jest.fn())
-jest.mock('../../app/processing', () => ({
-  start: jest.fn()
-}))
-jest.mock('../../app/messaging', () => ({
-  start: jest.fn(),
-  stop: jest.fn()
-}))
-
 const { setup: mockSetup } = require('../../app/insights')
+jest.mock('../../app/processing')
 const { start: mockProcessingStart } = require('../../app/processing')
+jest.mock('../../app/messaging')
 const { start: mockMessagingStart } = require('../../app/messaging')
+
 jest.mock('../../app/storage.js')
 const { initialiseContainers: mockInitialiseContainers } = require('../../app/storage')
 
-describe('app', () => {
+const startApp = require('../../app')
+
+describe('app start', () => {
   beforeEach(() => {
     jest.clearAllMocks()
+  })
 
+  test('setup insights', async () => {
     jest.isolateModules(() => {
       require('../../app')
     })
-  })
-
-  test('should setup insights', () => {
     expect(mockSetup).toHaveBeenCalled()
   })
 
-  test('should initialise containers', () => {
-    expect(mockInitialiseContainers).toHaveBeenCalled()
+  test('initialises containers when active is true', async () => {
+    processingConfig.processingActive = true
+    await startApp()
+    expect(mockInitialiseContainers).toHaveBeenCalledTimes(1)
   })
 
-  test('should start processing', () => {
-    expect(mockProcessingStart).toHaveBeenCalled()
+  test('does not initialise containers when active is false', async () => {
+    processingConfig.processingActive = false
+    await startApp()
+    expect(mockInitialiseContainers).toHaveBeenCalledTimes(0)
   })
 
-  test('should start messaging', () => {
-    expect(mockMessagingStart).toHaveBeenCalled()
+  test('starts processing when active is true', async () => {
+    processingConfig.processingActive = true
+    await startApp()
+    expect(mockProcessingStart).toHaveBeenCalledTimes(1)
+  })
+
+  test('does not start processing when active is false', async () => {
+    processingConfig.processingActive = false
+    await startApp()
+    expect(mockProcessingStart).toHaveBeenCalledTimes(0)
+  })
+
+  test('starts messaging when active is true', async () => {
+    processingConfig.processingActive = true
+    await startApp()
+    expect(mockMessagingStart).toHaveBeenCalledTimes(1)
+  })
+
+  test('does not start messaging when active is false', async () => {
+    processingConfig.processingActive = false
+    await startApp()
+    expect(mockMessagingStart).toHaveBeenCalledTimes(0)
+  })
+
+  test('does not log console.info when processingActive is true', async () => {
+    processingConfig.processingActive = true
+    const consoleInfoSpy = jest.spyOn(console, 'info').mockImplementation(() => {})
+    await startApp()
+    expect(consoleInfoSpy).not.toHaveBeenCalled()
+    consoleInfoSpy.mockRestore()
+  })
+
+  test('logs console.info when processingActive is false', async () => {
+    processingConfig.processingActive = false
+    const consoleInfoSpy = jest.spyOn(console, 'info').mockImplementation(() => {})
+    await startApp()
+    expect(consoleInfoSpy).toHaveBeenCalledWith(
+      expect.stringContaining('Processing capabilities are currently not enabled in this environment')
+    )
+    consoleInfoSpy.mockRestore()
   })
 })
